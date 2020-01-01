0faa0ee931cb8a49935c6fd2d9c132da720001d1
DIMASHPK <36626107+DIMASHPK@users.noreply.github.com>
2020-01-01 20:45:55 +0200

    ToDo list

---
+++ precache-manifest.1c01b1b2d9ce31b7a5a330fdb68fc316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76867acca663936dd2883190c216b4b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e691e9452dc0032973c3",
+    "url": "/static/css/main.1477b636.chunk.css"
+  },
+  {
+    "revision": "21bc7f4894a3c3393e9c",
+    "url": "/static/js/2.8c9f4fed.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8c9f4fed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e691e9452dc0032973c3",
+    "url": "/static/js/main.18032e8e.chunk.js"
+  },
+  {
+    "revision": "3a974dc9d2d01ebd1cc8",
+    "url": "/static/js/runtime-main.8e8986ee.js"
+  }
+]);
