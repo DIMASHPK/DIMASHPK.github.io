ed4d76a511b7bd9efc30dc4c77e1425e1caab299
dimashpk <shupimonstr@gmail.com>
2020-01-08 14:52:18 +0200

    add tic_toc

---
+++ tic_toc/precache-manifest.6bf10ad6b1a1fcc2e62a70741f469fed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "978eb67a56112f69547b8ba18cb548db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9575604f7620785f7888",
+    "url": "/static/css/2.037c9ca5.chunk.css"
+  },
+  {
+    "revision": "fdea90de1df954509315",
+    "url": "/static/css/main.ebda32f4.chunk.css"
+  },
+  {
+    "revision": "9575604f7620785f7888",
+    "url": "/static/js/2.5429f782.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5429f782.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fdea90de1df954509315",
+    "url": "/static/js/main.bdb245ed.chunk.js"
+  },
+  {
+    "revision": "bf48f33151f16aa809fe",
+    "url": "/static/js/runtime-main.bebd5310.js"
+  }
+]);
