bef3a507ea48e38f2abe86ffe472cf365c857c96
DIMASHPK <36626107+DIMASHPK@users.noreply.github.com>
2020-01-14 14:46:56 +0200

    Add files via upload

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.457cbd960d888f8fc482bd9338192aa8.js"
+  "/precache-manifest.6b47d726eafd0ebc5e0010d949fa8669.js"
 );
 
 self.addEventListener('message', (event) => {
