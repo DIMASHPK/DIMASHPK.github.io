769225908646c881708318595b73acbde042a075
DIMASHPK <36626107+DIMASHPK@users.noreply.github.com>
2020-01-19 19:36:54 +0200

    Add files via upload

---
+++ build/precache-manifest.5d6480fd2b8955b67f4ebe4d7ec96b7e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ed18cf799550ab3a7ba8a8059fd2fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cda4669770b53e8f2c51",
+    "url": "./static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b97f520e494f8f210136",
+    "url": "./static/css/main.a5842c80.chunk.css"
+  },
+  {
+    "revision": "cda4669770b53e8f2c51",
+    "url": "./static/js/2.0e8ac4f7.chunk.js"
+  },
+  {
+    "revision": "931f634827e16235d99e4ff139d33883",
+    "url": "./static/js/2.0e8ac4f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b97f520e494f8f210136",
+    "url": "./static/js/main.fb80f364.chunk.js"
+  },
+  {
+    "revision": "183bdabdec7dc54d749f",
+    "url": "./static/js/runtime-main.544078fa.js"
+  },
+  {
+    "revision": "17a1468e62195d0688a6f3bd12da2e92",
+    "url": "./static/media/Overpass-SemiBold.17a1468e.eot"
+  },
+  {
+    "revision": "17a1468e62195d0688a6f3bd12da2e92",
+    "url": "./static/media/Overpass-SemiBold.17a1468e.ttf"
+  },
+  {
+    "revision": "17a1468e62195d0688a6f3bd12da2e92",
+    "url": "./static/media/Overpass-SemiBold.17a1468e.woff"
+  },
+  {
+    "revision": "8669b8706bbbdd1482e2fccc4ed96850",
+    "url": "./static/media/SourceSansPro-Bold.8669b870.eot"
+  },
+  {
+    "revision": "8669b8706bbbdd1482e2fccc4ed96850",
+    "url": "./static/media/SourceSansPro-Bold.8669b870.ttf"
+  },
+  {
+    "revision": "8669b8706bbbdd1482e2fccc4ed96850",
+    "url": "./static/media/SourceSansPro-Bold.8669b870.woff"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "./static/media/SourceSansPro-Regular.c1678b46.eot"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "./static/media/SourceSansPro-Regular.c1678b46.ttf"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "./static/media/SourceSansPro-Regular.c1678b46.woff"
+  }
+]);
