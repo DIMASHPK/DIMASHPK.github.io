a60144113bb8f9bc328e6421fee48c1c920c3783
dimashpk <shupimonstr@gmail.com>
2020-02-07 02:10:13 +0200

    abz second test work

---
+++ abz/precache-manifest.d4d305d7d59049ef32c3094d7f225449.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23cacb22e6784e1bd98c9b5ad6ae151",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53ad663cd0576e1e0c3f",
+    "url": "/static/css/2.3d495d1e.chunk.css"
+  },
+  {
+    "revision": "8a5fd578a00f74d5524a",
+    "url": "/static/css/main.5087b50d.chunk.css"
+  },
+  {
+    "revision": "53ad663cd0576e1e0c3f",
+    "url": "/static/js/2.d833ab59.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.d833ab59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae6dd7edc9ea47025d77",
+    "url": "/static/js/3.cea7bf99.chunk.js"
+  },
+  {
+    "revision": "8a5fd578a00f74d5524a",
+    "url": "/static/js/main.1e2ead5d.chunk.js"
+  },
+  {
+    "revision": "41f1fe0ab6bc4c6e288a",
+    "url": "/static/js/runtime-main.23f065fc.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.eot"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.woff"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/static/media/OpenSans-SemiBold.ba5cde21.eot"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/static/media/OpenSans-SemiBold.ba5cde21.woff"
+  },
+  {
+    "revision": "4ea26cd5e7f64894d6c2451446f7dda5",
+    "url": "/static/media/PTSans-Regular.4ea26cd5.eot"
+  },
+  {
+    "revision": "4ea26cd5e7f64894d6c2451446f7dda5",
+    "url": "/static/media/PTSans-Regular.4ea26cd5.ttf"
+  },
+  {
+    "revision": "4ea26cd5e7f64894d6c2451446f7dda5",
+    "url": "/static/media/PTSans-Regular.4ea26cd5.woff"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "/static/media/SourceSansPro-Regular.c1678b46.eot"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "/static/media/SourceSansPro-Regular.c1678b46.ttf"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "/static/media/SourceSansPro-Regular.c1678b46.woff"
+  }
+]);
