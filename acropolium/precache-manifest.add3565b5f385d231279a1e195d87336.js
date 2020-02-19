47c09d43ac8234efc61f1f6a451387c0baef87df
dimashpk <shupimonstr@gmail.com>
2020-02-19 18:02:28 +0200

    acropoliu, test work

---
+++ acropolium/precache-manifest.add3565b5f385d231279a1e195d87336.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c904fe7fe543f3cba2af87add19a12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71d43b694ab2272f5261",
+    "url": "/static/css/2.4121280a.chunk.css"
+  },
+  {
+    "revision": "ecc9ee5ff638e4cfabed",
+    "url": "/static/css/main.9ba855f5.chunk.css"
+  },
+  {
+    "revision": "71d43b694ab2272f5261",
+    "url": "/static/js/2.f25b70bc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.f25b70bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecc9ee5ff638e4cfabed",
+    "url": "/static/js/main.d7455115.chunk.js"
+  },
+  {
+    "revision": "c67e7fc494555f284ce1",
+    "url": "/static/js/runtime-main.a43b6eb2.js"
+  },
+  {
+    "revision": "31d98c8d4ef07798144ab14521b0b818",
+    "url": "/static/media/OPTIHobble-OldStyle.31d98c8d.eot"
+  },
+  {
+    "revision": "31d98c8d4ef07798144ab14521b0b818",
+    "url": "/static/media/OPTIHobble-OldStyle.31d98c8d.ttf"
+  },
+  {
+    "revision": "31d98c8d4ef07798144ab14521b0b818",
+    "url": "/static/media/OPTIHobble-OldStyle.31d98c8d.woff"
+  }
+]);
