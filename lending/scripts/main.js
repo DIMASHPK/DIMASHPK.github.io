23e54ad0bc519be8a93fee6a75fee3d3cf677c55
dimashpk <shupimonstr@gmail.com>
2020-03-29 18:45:18 +0300

    changes for lending

--- lending/scripts/main.js
+++ lending/scripts/main.js
@@ -4,6 +4,15 @@ const submit = document.querySelector(".modalForm button");
 const passwordInput = document.querySelector('input[id="password"]');
 const confirmInput = document.querySelector('input[id="confirm"]');
 const error = document.querySelector(".error");
+const modalForBuy = document.querySelector(".modalForBuy");
+const buyButton = document.querySelector(".modalForBuy .submit_button");
+const burgerMenu = document.querySelector(".burger button");
+const navPanel = document.querySelector(".navigation");
+
+burgerMenu.addEventListener("click", () => {
+  burgerMenu.classList.toggle("active");
+  navPanel.classList.toggle("open");
+});
 
 openModalButtons.forEach(button => {
   button.addEventListener("click", e => {
@@ -15,11 +24,16 @@ openModalButtons.forEach(button => {
 submit.addEventListener("click", e => {
   e.preventDefault();
   modal.style.display = "none";
+  modalForBuy.style.display = "block";
 });
 
 [passwordInput, confirmInput].forEach(input =>
   input.addEventListener("blur", validation)
 );
+
+buyButton.addEventListener("click", () => {
+  modalForBuy.style.display = "none";
+});
 
 function validation() {
   if (
